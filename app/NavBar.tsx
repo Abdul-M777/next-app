8ae3ca1f52ae750f09d9a8d1b0e8f5460040df2c
Abdulmuaz Alshaikhli <slmnshukur1993@gmail.com>
2025-04-05 20:04:39 +0200

    Authentication with google and manuel login

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,34 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-5 space-x-3">
       <Link className="mr-5" href="/">
         Next.js
       </Link>
       <Link className="mr-5" href="/users">
         Users
       </Link>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "authenticated" && (
+        <div>
+          {session.user?.name}{" "}
+          <Link href={"/api/auth/signout"} className="ml-3">
+            Sign Out
+          </Link>
+        </div>
+      )}
+      {status === "unauthenticated" && (
+        <Link className="mr-5" href="/api/auth/signin">
+          Sign In
+        </Link>
+      )}
     </div>
   );
 };
---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import middleware from "next-auth/middleware";
+
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/new-page", request.url));
+// }
+
+export default middleware;
+
+export const config = {
+  matcher: ["/users/:id*"],
+};
